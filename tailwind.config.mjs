79f653735951bbc96beb27ed8a77d006cebed795
00Alexis <00Alexis00@gmail.com>
2024-03-13 16:34:27 -0600

    tailwindcss

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.astro'],
+	theme: {
+		extend: {
+			colors:{
+				primaryBlue:"#DBF227",
+			}
+		},
+	},
+	plugins: [],
+}
